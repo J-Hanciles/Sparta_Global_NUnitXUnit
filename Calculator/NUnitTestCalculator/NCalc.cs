813684eb4715090c048d7e6c548091006eb036a4
Jerome Hanciles <jhanciles@spartaglobal.com>
2019-02-20 09:07:11 +0000

    NUnit completed

--- Calculator/NUnitTestCalculator/NCalc.cs
+++ Calculator/NUnitTestCalculator/NCalc.cs
@@ -5,17 +5,62 @@ namespace Tests
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
         [Test]
-        public void Test1()
+        public void TestAdd()
         {
-            Assert.Pass();
+            CalculatorApp calc = new CalculatorApp();
+            double expected = 6.0;
+            double actual = calc.Add(4.0, 2.0);
+            Assert.AreEqual(expected, actual);
         }
-
+        [Test]
+        public void TestDivide()
+        {
+            CalculatorApp calc = new CalculatorApp();
+            double expected = 2.0;
+            double actual = calc.Divide(4.0, 2.0);
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void TestSubtract()
+        {
+            CalculatorApp calc = new CalculatorApp();
+            double expected = 3.0;
+            double actual = calc.Subtract(5.0, 2.0);
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void TestMultiply()
+        {
+            CalculatorApp calc = new CalculatorApp();
+            double expected = 10.0;
+            double actual = calc.Multiply(5.0, 2.0);
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void TestModular()
+        {
+            CalculatorApp calc = new CalculatorApp();
+            double expected = 0.0;
+            double actual = calc.Modular(6.0, 3.0);
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void TestCircleA()
+        {
+            CalculatorApp calc = new CalculatorApp();
+            double expected = 12.568;
+            double actual = calc.CircleA(2.0);
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void TestTriangleA()
+        {
+            CalculatorApp calc = new CalculatorApp();
+            double expected = 18;
+            double actual = calc.TriangleA(4.0, 9.0);
+            Assert.AreEqual(expected, actual);
+        }
 
     }
 }

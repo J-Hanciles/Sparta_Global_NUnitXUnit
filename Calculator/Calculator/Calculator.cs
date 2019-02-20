dc6e3b99c6171f2da97672778c0351188d0cd740
Jerome Hanciles <jhanciles@spartaglobal.com>
2019-02-20 08:57:35 +0000

    XUnit completed

--- Calculator/Calculator/Calculator.cs
+++ Calculator/Calculator/Calculator.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Calculator
 {
-    class Calculator
+    public class CalculatorApp
     {
         public static void Main(string[] args)
         {
--- Calculator/XUnitTestCaculator/XCalc.cs
+++ Calculator/XUnitTestCaculator/XCalc.cs
@@ -7,15 +7,60 @@ namespace XUnitTestCaculator
     public class XCalc
     {
         [Fact]
-        public void Test1()
+        public void TestAdd()
         {
-
+            CalculatorApp calc = new CalculatorApp();
+            double expected = 6.0;
+            double actual = calc.Add(4.0, 2.0);
+            Assert.Equal(expected, actual);
         }
-        
         [Fact]
-        public void Test2()
+        public void TestDivide()
         {
-
+            CalculatorApp calc = new CalculatorApp();
+            double expected = 2.0;
+            double actual = calc.Divide(4.0, 2.0);
+            Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void TestSubtract()
+        {
+            CalculatorApp calc = new CalculatorApp();
+            double expected = 3.0;
+            double actual = calc.Subtract(5.0, 2.0);
+            Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void TestMultiply()
+        {
+            CalculatorApp calc = new CalculatorApp();
+            double expected = 10.0;
+            double actual = calc.Multiply(5.0, 2.0);
+            Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void TestModular()
+        {
+            CalculatorApp calc = new CalculatorApp();
+            double expected = 0.0;
+            double actual = calc.Modular(6.0, 3.0);
+            Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void TestCircleA()
+        {
+            CalculatorApp calc = new CalculatorApp();
+            double expected = 12.568;
+            double actual = calc.CircleA(2.0);
+            Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void TestTriangleA()
+        {
+            CalculatorApp calc = new CalculatorApp();
+            double expected = 18;
+            double actual = calc.TriangleA(4.0, 9.0);
+            Assert.Equal(expected, actual);
         }
     }
 }

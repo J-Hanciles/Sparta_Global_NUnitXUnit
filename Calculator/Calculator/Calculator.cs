8f3871e2b9208f0ea2967185f7f2b76c4b5a3350
Jerome Hanciles <jhanciles@spartaglobal.com>
2019-02-20 08:41:53 +0000

    Base files added

---
+++ Calculator/Calculator/Calculator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Calculator
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+        public double Add(double num1, double num2)
+        {
+            double result = num1 + num2;
+            return result;
+        }
+        public double Divide(double num1, double num2)
+        {
+            double result = num1 / num2;
+            return result;
+        }
+        public double Subtract(double num1, double num2)
+        {
+            double result = num1 - num2;
+            return result;
+        }
+        public double Multiply(double num1, double num2)
+        {
+            double result = num1 * num2;
+            return result;
+        }
+        public double Modular(double num1, double num2)
+        {
+            double result = num1 % num2;
+            return result;
+        }
+        public double CircleA(double num1)
+        {
+            double result = 3.142 * (num1 * num1);
+            return result;
+        }
+        public double TriangleA(double num1, double num2)
+        {
+            double result = (num1 * num2) / 2;
+            return result;
+        }
+    }
+}
---
+++ Calculator/XUnitTestCaculator/XCalc.cs
@@ -0,0 +1,21 @@
+using System;
+using Xunit;
+using Calculator;
+
+namespace XUnitTestCaculator
+{
+    public class XCalc
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+        
+        [Fact]
+        public void Test2()
+        {
+
+        }
+    }
+}
